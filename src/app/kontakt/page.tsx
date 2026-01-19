030554c4c3b3dd468befed0719929bf9853f004b
Arkadiusz Piersiak <arkadiusz.piersiak@gmail.com>
2026-01-19 13:39:10 +0100

    finished team section for desktop

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,3 +1,4 @@
+import TeamSection from "@/components/contact-page/TeamSection/TeamSection";
 import SubHero from "@/components/layout/SubHero/SubHero";
 import CTA from "@/components/ui/Cta";
 
@@ -12,10 +13,11 @@ export default function Home() {
       <SubHero heading={heroData.heading} subheading={heroData.subheading}>
         <div className="w-50">
           <CTA variant="red">
-            <span className="font-thin text-sm">Napisz do nas</span>
+            <span className="font text-sm">Napisz do nas</span>
           </CTA>
         </div>
       </SubHero>
+      <TeamSection />
     </div>
   );
 }
