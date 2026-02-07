42c68f6bca148205250a803ed89477f605d4af50
Arkadiusz Piersiak <arkadiusz.piersiak@gmail.com>
2026-02-07 21:39:48 +0100

    added honeypot and success/error handling

--- src/components/pages/contact-page/contact-page/ContactForm/useSubmit.tsx
+++ src/components/pages/contact-page/contact-page/ContactForm/useSubmit.tsx
@@ -1,24 +1,55 @@
+import { useState } from "react";
 import { buildPayload } from "./buildPayload";
 import { FormValues } from "./ContactForm";
+import { NextResponse } from "next/server";
 
 export const useSubmit = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState(false);
+
   const onSubmit = async (data: FormValues) => {
-    const payload = buildPayload(data);
+    try {
+      setIsLoading(true);
+      setError(null);
+      setSuccess(false);
 
-    const res = await fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+      const payload = buildPayload(data);
 
-    if (!res.ok) {
-      const errorData = await res.json();
-      alert(`Błąd: ${errorData.error}`);
+      // Honeypot check
+      const { website } = data;
+      if (website && website.trim().length > 0) {
+        setIsLoading(false);
+        setSuccess(true);
+        console.warn("Bot detected, submission ignored.");
+        return NextResponse.json({ ok: true });
+      }
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      const json = await res.json();
+
+      // Error handling
+      if (!res.ok) {
+        setError(
+          json.message ||
+            "Nie udało się wysłać wiadomości. Spróbuj ponownie później.",
+        );
+      }
+      setSuccess(true);
+      return;
+    } catch {
+      setIsLoading(false);
+      setError("Brak połączenia z serwerem. Spróbuj ponownie później.");
+    } finally {
+      setIsLoading(false);
     }
-    alert("Wiadomość została wysłana!");
   };
 
-  return { onSubmit };
+  return { onSubmit, isLoading, error, success };
 };
