fa6e375b65495480452cffd8e023e2ff9ca9a437
Arkadiusz Piersiak <arkadiusz.piersiak@gmail.com>
2026-02-09 18:43:08 +0100

    test

--- src/components/pages/landing/Hero.tsx
+++ src/components/pages/landing/Hero.tsx
@@ -5,7 +5,19 @@ export default function Hero() {
   return (
     <section className="px-4 py-2 border  text-(--color-primary) relative">
       <div className="relative  ">
-        <div className="flex flex-col items-center justify-center h-full gap-12 text-center py-35  bg-gray-900  h-full clip-hero ">
+        <div className="flex flex-col items-center justify-center h-full gap-12 text-center py-35 clip-hero ">
+          <video
+            className="absolute z-[-1] inset-0 h-full w-full object-cover"
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="none"
+          >
+            <source src="/video/hero.webm" type="video/webm" />
+            <source src="/video/hero.mp4" type="video/mp4" />
+          </video>
+          <div className="absolute inset-0 bg-black/30" />
           <div className="w-66">
             <ColorLogo />
           </div>
