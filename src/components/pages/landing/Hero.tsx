a264ac4111123e3abfe1afbc9b04e49374b4fb0d
Arkadiusz Piersiak <arkadiusz.piersiak@gmail.com>
2026-02-09 23:07:47 +0100

    added heading for map section and poster image for hero video

--- src/components/pages/landing/Hero.tsx
+++ src/components/pages/landing/Hero.tsx
@@ -1,12 +1,23 @@
 import ColorLogo from "@/components/ui/ColorLogo";
 import CTA from "@/components/ui/cta";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className="px-4 py-2 border  text-(--color-primary) relative">
       <div className="relative  ">
         <div className=" clip-hero ">
-          <video
+          <div className="absolute z-[-1] inset-0">
+            <Image
+              src="/images/poster.jpg"
+              alt=""
+              fill
+              priority
+              className="object-cover"
+            />
+            <div className="absolute inset-0 bg-[#000000]/60" />
+          </div>
+          {/* <video
             className="absolute z-[-1] inset-0 h-full w-full object-cover"
             autoPlay
             muted
@@ -14,9 +25,9 @@ export default function Hero() {
             playsInline
             preload="none"
           >
-            {/* <source src="/video/hero.webm" type="video/webm" /> */}
+            <source src="/video/hero.webm" type="video/webm" />
             <source src="/video/hero.mp4" type="video/mp4" />
-          </video>
+          </video> */}
           <div className="absolute inset-0 bg-[#000000]/60" />
           <div className="relative   py-35 flex flex-col items-center justify-center h-full gap-12 text-center z-10">
             <div className="w-66">
