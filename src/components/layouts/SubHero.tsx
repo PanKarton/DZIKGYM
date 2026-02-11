9b0268d34689a90ed4de6d62b625ad67f025da42
Arkadiusz Piersiak <arkadiusz.piersiak@gmail.com>
2026-02-11 22:53:22 +0100

    added sub-hero bg image

--- src/components/layouts/SubHero.tsx
+++ src/components/layouts/SubHero.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function SubHero({
   heading,
   subheading,
@@ -9,13 +11,23 @@ export default function SubHero({
 }) {
   return (
     <section className="px-4 py-2 border  text-(--color-primary) relative h-106">
-      <div className="relative  ">
-        <div className="flex flex-col items-center justify-center h-full gap-8 text-center pt-35 pb-20 sub-hero-gradient h-full clip-sub-hero ">
-          <h1 className="font-black text-[3.875rem] italic leading-none">
-            {heading}
-          </h1>
-          <p className="font-thin">{subheading}</p>
-          {children}
+      <div className="relative ">
+        <div className="   h-full clip-sub-hero ">
+          <Image
+            src="/images/sub-hero-pattern.png"
+            alt="Tło sekcji sub-hero"
+            fill
+            className="object-cover z-1"
+          />
+          <div className="absolute inset-0 sub-hero-gradient z-2"></div>
+          <div className="absolute inset-0 bg-black z-0"></div>
+          <div className="relative z-3 flex flex-col items-center justify-center gap-8 text-center pt-35 pb-20">
+            <h1 className="font-black text-[3.875rem] italic leading-none">
+              {heading}
+            </h1>
+            <p className="font-thin text-xl">{subheading}</p>
+            {children}
+          </div>
         </div>
       </div>
     </section>
